a69bb5e5415f47acf74f36730b08d35fb7b070cb
Tanay Van <tanayvan@Tanays-MacBook-Pro.local>
2023-04-18 20:18:43 -0400

    minor change

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-const express = require('express')
-const mongoose=require('mongoose')
-const bodyParser=require('body-parser')
-const cookieParser=require('cookie-parser')
-const cors=require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app=express()
+const app = express();
 
 //middlewares
-app.use(bodyParser.json())
-app.use(cors())
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
 
 //my Routes
-const authRoutes=require('./routes/auth')
-const userRoutes= require('./routes/user')
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 //DataBAse Connection
-mongoose.connect("mongodb://localhost:27017/Twitter20",{useNewUrlParser:true,useUnifiedTopology: true ,useCreateIndex:true})
-.then(()=>{
-    console.log("Database Connected")
-}).catch(()=>{
-    console.log("Error Connecting to Database")
-})
-
+mongoose
+  .connect("mongodb://localhost:27017/Twitter20", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch(() => {
+    console.log("Error Connecting to Database");
+  });
 
 //Set Routes
-app.use("/",(req,res) => {
-res.send(`
+app.get("/", (req, res) => {
+  res.send(`
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -438,13 +443,12 @@ header {
 
   <script src="script.js"></script>
 </html>
-`)
-})
-app.use('/api',authRoutes)
-app.use('/api',userRoutes)
+`);
+});
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
-
-const port=process.env.PORT || 7010
-app.listen(port,() => {
-    console.log(`Server Started on ${port}`)
-})
+const port = process.env.PORT || 7010;
+app.listen(port, () => {
+  console.log(`Server Started on ${port}`);
+});
